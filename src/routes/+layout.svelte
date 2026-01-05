9ee0ea574e38ea1563325f2f12df3684b0deb69e
Ben Davidson <bfd4542@gmail.com>
2026-01-05 17:44:41 -0500

    Added favicon and SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import DebugOverlay from '$lib/components/DebugOverlay.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
 
